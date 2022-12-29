1a54c81c0e8778e64f79685c4c7a8c65c72b9399
Nate Ahiagbedey <nahiagbedey@gmail.com>
2022-12-29 12:52:03 -0500

    Initial setup
    created placement for header, disease, and map. Map zoom and color change added on svg

---
+++ idrp-sd/src/component/diseaseData/DataView.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./DataView.css";
+import "../global.css";
+
+export default function DataView() {
+  return <div className="data-section">DataView</div>;
+}
---
+++ idrp-sd/src/component/header/TitleHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TitleHeader.css";
+
+export default function TitleHeader() {
+  return (
+    <div>
+      <h1>Infectious Disease Risk Predictor</h1>
+    </div>
+  );
+}
